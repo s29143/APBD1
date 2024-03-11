2eb1e232236908567ae595310602fecb95db6a76
s29143 <s29143@pjwstk.edu.pl>
2024-03-11 13:41:20 +0100

    added max method

--- APBD1/Program.cs
+++ APBD1/Program.cs
@@ -3,6 +3,7 @@
     public static void Main()
     {
         Console.WriteLine(Average([1, 2, 3, 4]));
+        Console.WriteLine(Max([1, 2, 5, 3, 4]));
     }
 
     public static double Average(int[] numbers)
@@ -10,5 +11,16 @@
         double sum = 0;
         numbers.Sum(x => sum += x);
         return sum / numbers.Length;
+    }
+
+    public static int Max(int[] numbers)
+    {
+        int max = 0;
+        foreach (var item in numbers)
+        {
+            if(item > max)
+                max = item;
+        }
+        return max;
     }
 }
