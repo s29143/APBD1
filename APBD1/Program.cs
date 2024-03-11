e077b3188d71362e9e3282426f22d0d9a500cba2
s29143 <s29143@pjwstk.edu.pl>
2024-03-11 13:36:09 +0100

    added average method

--- APBD1/Program.cs
+++ APBD1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Modyfikacja 3!");
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(Average([1, 2, 3, 4]));
+    }
+
+    public static double Average(int[] numbers)
+    {
+        double sum = 0;
+        numbers.Sum(x => sum += x);
+        return sum / numbers.Length;
+    }
+}
